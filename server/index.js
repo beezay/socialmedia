52a5be771400753740b376037a942c0cb26d45cd
beezay bohora <bijay.okur@gmail.com>
2020-12-12 18:36:49 +0545

    server created

--- server/index.js
+++ server/index.js
@@ -3,3 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+//Mongoose
+const CONNECTION_URL = 'mongodb+srv://admin:admin123@cluster0.7zq4e.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    })
+    .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
